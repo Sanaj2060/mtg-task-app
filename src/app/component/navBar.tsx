f93cfeb3075dc15611ded2d3c6dbf6f909879fb2
s2060 <Sanaj.2060@gmail.com>
2024-08-26 09:42:38 +0530

    Update navBar.tsx
    close signout if clicked outside

--- src/app/component/navBar.tsx
+++ src/app/component/navBar.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Session } from "next-auth";
 import { signOut } from "next-auth/react";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 
 const NavBar = (props: Session) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleDropdownToggle = () => {
     setIsDropdownOpen((prev) => !prev);
@@ -15,16 +16,28 @@ const NavBar = (props: Session) => {
     signOut();
   };
 
+  // Close dropdown if clicked outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="p-4 flex justify-between w-full">
       <div className="text-lg font-semibold">MTGTODO</div>
-      <div>
-        {/* <img 
-          src={props.user?.image ? props.user.image : "../../public/next.svg"}
-          className="max-h-36 rounded-full w-9"
-          alt={`profile photo of ${props.user?.name}`}
-          onClick={handleDropdownToggle}
-        /> */}
+      <div ref={dropdownRef}>
         <Image 
           src={props.user?.image ? props.user.image : "/next.svg"}
           className="max-h-36 rounded-full w-9"
