630fcf22e8e392c784769b4a4cda3b05b34b4875
Angom Oson Singh <angomosn64@gmail.com>
2024-09-03 00:16:50 +0530

    furnish registration page

--- src/app/component/taskCard.tsx
+++ src/app/component/taskCard.tsx
@@ -4,24 +4,31 @@ import Image from "next/image";
 import { useState } from "react";
 import { FormatRelativeDate, relativeDate } from "../api/lib/dateAsDay";
 
-const TaskCard: React.FC<{ id: string, duedate: Date }> = ({ id, duedate }) => {
+const TaskCard: React.FC<{ id: string; duedate: Date }> = ({ id, duedate }) => {
   const [show, setShow] = useState(true);
   const handleClick = () => {
     // console.log("Show Set");
     setShow((prevShow) => !prevShow);
   };
-  const relativeDate: relativeDate = duedate ? FormatRelativeDate(duedate) : {dateText: "", textColor: ""}
+  const relativeDate: relativeDate = duedate
+    ? FormatRelativeDate(duedate)
+    : { dateText: "", textColor: "" };
   return (
     <div className="flex flex-col">
       {show && (
         <>
           <div className="flex mb-3">
             <div className="flex items-start pt-1">
-              <input type="checkbox" name="option1" value="value1" onClick={handleClick}/>
+              <input
+                type="checkbox"
+                name="option1"
+                value="value1"
+                onClick={handleClick}
+              />
             </div>
 
             <div className="flex flex-col pl-3 gap-1">
-              <p>{id} - Create to-do app</p>
+              <p>{id}</p>
               <p className="text-sm text-gray-500">To-do app for mateng</p>
               <div className="flex">
                 <Image
@@ -31,7 +38,9 @@ const TaskCard: React.FC<{ id: string, duedate: Date }> = ({ id, duedate }) => {
                   className="block w-4 h-4 text-green-500 mr-2"
                   alt="calendar"
                 />
-                <p className={relativeDate.textColor}>{relativeDate.dateText}</p>
+                <p className={relativeDate.textColor}>
+                  {relativeDate.dateText}
+                </p>
               </div>
             </div>
           </div>
--- src/app/component/taskCardWrapper.tsx
+++ src/app/component/taskCardWrapper.tsx
@@ -2,19 +2,19 @@
 import { getAllTasksWithUserName, getAllTasks } from "../api/lib/db";
 import TaskCard from "./taskCard";
 
-export default async function TaskCardWrapper({id}: any){
-    const tasks = await getAllTasksWithUserName(id);
-    // console.log(id)
-    // console.log(tasks)
-    return (
-        <div>
-            {tasks?.tasks.map((task, key) =>
-                task.duedate ? (
-                    <TaskCard key={task.id} id={task.id} duedate={task.duedate}/>
-                ) : (
-                    <TaskCard key={task.id} id={task.id} duedate={new Date()}/>
-                )
-            )}
-        </div>
-    )
+export default async function TaskCardWrapper({ id }: any) {
+  const tasks = await getAllTasksWithUserName(id);
+  // console.log(id)
+  // console.log(tasks)
+  return (
+    <div>
+      {tasks?.tasks.map((task, key) =>
+        task.duedate ? (
+          <TaskCard key={task.id} id={task.title} duedate={task.duedate} />
+        ) : (
+          <TaskCard key={task.id} id={task.title} duedate={new Date()} />
+        )
+      )}
+    </div>
+  );
 }
